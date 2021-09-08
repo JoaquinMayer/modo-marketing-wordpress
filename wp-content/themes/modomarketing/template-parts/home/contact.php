97531202f5d015917e29f46bb75902a1344f830d
Joaquin Mayer <joaquin@vexels.com>
2021-09-08 19:30:09 -0300

    Add home contact

---
+++ wp-content/themes/modomarketing/template-parts/home/contact.php
@@ -0,0 +1,45 @@
+<section class="row mm-background-form">
+    <div class="col mt-3">
+        <h1 class="h1 my-3 text-primary mm-header-1 d-lg-none">
+            Dos perspectivas sobre el marketing, los negocios y la vida.
+        </h1>
+        <div class="d-flex justify-content-center my-5">
+            <img width="130" src="<?php echo esc_url(get_template_directory_uri()) . '/assets/images/switch.png' ?> " srcset="<?php echo esc_url(get_template_directory_uri()) . '/assets/images/switch.png' ?> 1x, <?php echo esc_url(get_template_directory_uri()) . '/assets/images/switch@2x.png' ?> 2x" alt="switch modo marketing" class="img-fluid d-md-none" />
+        </div>
+        <div class="mx-3 mb-5">
+            <h2 class="h2 text-secondary mm-header-2 text-md-center">
+                ¡Activa tu Modo Marketing!
+            </h2>
+            <p class="mm-text-1 text-md-center">
+                Postulate aquí para participar en el poodcast y recibir una
+                asesoría en vivo.<br class="d-none d-md-block" />
+                Cuéntanos sobre tuempresa o proyecto, y sobre todo, qué te
+                preocupa o quisieras mejorar.
+            </p>
+            <div class="border-0 card col col-md-3 m-auto shadow">
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <input type="text" name="name" class="form-control bg-light" id="nameInput" placeholder="Escribe aquí tu nombre" />
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" name="email" class="form-control bg-light" id="emailInput" placeholder="Escribe aquí tu email" />
+                        </div>
+                        <div class="mb-3">
+                            <textarea name="message" class="form-control bg-light" id="messageInput" placeholder="Cuéntanos sobre tu negocio" rows="6"></textarea>
+                        </div>
+                        <input type="submit" class="
+                      my-md-3
+                      mm-btn-submit
+                      btn btn-secondary
+                      w-100
+                      text-uppercase
+                      rounded-pill
+                      border-0
+                    " value="Participar" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
