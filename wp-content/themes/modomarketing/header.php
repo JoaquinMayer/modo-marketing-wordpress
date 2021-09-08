c8d87d1cd366e19a2cf96372450c3583985ae556
Joaquin Mayer <joaquin@vexels.com>
2021-09-07 21:51:38 -0300

    Update

--- wp-content/themes/modomarketing/header.php
+++ wp-content/themes/modomarketing/header.php
@@ -19,7 +19,15 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 	<?php wp_head(); ?>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -27,7 +35,7 @@
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'twentytwentyone'); ?></a>
 
-		<?php get_template_part('template-parts/header/site-header'); ?>
+		<?php get_template_part('template-parts/header/header-primary'); ?>
 
 		<div id="content" class="site-content">
 			<div id="primary" class="content-area">
---
+++ wp-content/themes/modomarketing/template-parts/header/header-primary.php
@@ -0,0 +1,119 @@
+<header class="d-flex flex-column mm-header px-lg-4 mm-h-800 pt-lg-5">
+    <nav class="navbar navbar-light">
+        <div class="container-fluid p-lg-0">
+            <button class="navbar-toggler mm-button-menu d-lg-none" id="mm-open-menu" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <img src="/img/menu-icon.png" alt="logo modo marketing" srcset="/img/menu-icon.png 1x, /img/menu-icon@2x.png 2x" class="d-inline-block align-text-top" />
+            </button>
+            <a class="navbar-brand m-0 p-0" href="#">
+                <img src="/img/logo.png" width="160" alt="logo modo marketing" srcset="/img/logo.png 1x, /img/logo@2x.png 2x" class="d-inline-block align-text-top img-fluid" />
+            </a>
+        </div>
+    </nav>
+    <div class="
+          d-flex
+          justify-content-center justify-content-lg-end
+          mt-5
+          position-relative
+          mm-h-800
+        ">
+        <img src="./img/raquel-diego.png" srcset="./img/raquel-diego.png 1x, ./img/raquel-diego@2x.png 2x" alt="raquel y diego" class="img-fluid d-lg-none" />
+        <img src="./img/raquel-diego-desktop.png" srcset="
+            ./img/raquel-diego-desktop.png    1x,
+            ./img/raquel-diego-desktop@2x.png 2x
+          " alt="raquel y diego" class="img-fluid d-none d-lg-block mm-raquel-diego" />
+        <div class="
+            mm-card-player
+            position-absolute
+            bottom-0
+            d-flex
+            align-items-center
+            d-lg-none
+          ">
+            <p class="mb-0 pe-4 mm-font-display-bold">
+                Escuchar <br />
+                Podcast ahora
+            </p>
+            <img src="./img/icon-play.png" class="mm-icon-play" srcset="./img/icon-play.png 1x, ./img/icon-play@2x.png 2x" alt="icono de play" />
+        </div>
+        <div id="mm-nav" class="
+            bottom-0
+            d-lg-flex d-none
+            flex-column
+            h-100
+            position-absolute
+            w-100
+            justify-content-between
+          ">
+            <div class="col d-flex align-items-center">
+                <h1 class="col-lg-6 mm-header-lg-1">
+                    Dos perspectivas sobre el <br />marketing, los negocios y la vida.
+                </h1>
+            </div>
+            <div class="col">
+                <div class="card col-lg-6 col-xl-4 mm-card-podcast">
+                    <div class="card-header d-flex justify-content-between bg-white">
+                        <p class="m-0">PODCAST // ÚLTIMO EPISODIO</p>
+                        <a href="" class="mm-link-podcast">Ver todos los episodios</a>
+                    </div>
+                    <div class="card-body d-flex justify-content-between">
+                        <div class="">
+                            <img src="./img/cover-podcast.jpg" alt="" srcset="" />
+                        </div>
+                        <div class="ms-2">
+                            <h3 class="card-text mm-title-podcast">
+                                El marketing ya no es lo que era ⎢ MODO MARKETING Ep. 1
+                            </h3>
+                            <a href="#" class="
+                      btn
+                      rounded-pill
+                      d-inline-flex
+                      align-items-center
+                      justify-content-start
+                      pe-3
+                      bg-green
+                    "><img src="./img/spotify-icon.png" alt="" class="me-2" />
+                                Escuchar Ahora</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav class="w-100">
+                <ul class="list-unstyled bg-white d-flex p-2 mm-menu">
+                    <li class="col">
+                        <a href="/" class="
+                    text-center text-uppercase
+                    w-100
+                    d-block
+                    mm-button-menu-desktop
+                    active
+                  ">Inicio</a>
+                    </li>
+                    <li class="col">
+                        <a href="/episodios.html" class="
+                    text-center text-uppercase
+                    w-100
+                    d-block
+                    mm-button-menu-desktop
+                  ">Episodios</a>
+                    </li>
+                    <li class="col">
+                        <a href="/materiales.html" class="
+                    text-center text-uppercase
+                    w-100
+                    d-block
+                    mm-button-menu-desktop
+                  ">Materiales</a>
+                    </li>
+                    <li class="col">
+                        <a href="/articulos.html" class="
+                    text-center text-uppercase
+                    w-100
+                    d-block
+                    mm-button-menu-desktop
+                  ">Articulos</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
